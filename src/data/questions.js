b437ae7a3a060902b4cb253f8860ebe720a779df
annakarina <annapacheco203@gmail.com>
2024-09-16 15:09:50 -0300

    up

---
+++ src/data/questions.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    question: "Qual é o nome do protagonista do jogo Grand Theft Auto: Vice City?",
+    options: [
+      "Tommy Vercetti",
+      "Carl CJ Johnson",
+      "Niko Bellic",
+      "Michael De Santa",
+    ],
+    answer: "Tommy Vercetti",
+  },
+  {
+    question: "Em qual cidade fictícia é ambientado o jogo Grand Theft Auto: San Andreas?",
+    options: [" Liberty City", "Vice City", "San Fierro", "Los Santos"],
+    answer: "Los Santos",
+  },
+  {
+    question: " Qual é o nome da organização criminosa que o jogador deve enfrentar em Grand Theft Auto V?",
+    options: ["The Lost MC", "The Ballas", "The FIB", "The Triads"],
+    answer: "The FIB",
+  },
+  {
+    question: "Qual foi o primeiro jogo da série Grand Theft Auto a utilizar um mundo aberto tridimensional?",
+    options: ["Grand Theft Auto", "Grand Theft Auto II", "Grand Theft Auto III", "Grand Theft Auto: Vice City"],
+    answer: "Grand Theft Auto III",
+  },
+  {
+    question:
+      "Em Grand Theft Auto IV, qual é o nome do primo de Niko Bellic que o chama para vir para Liberty City?",
+    options: ["Roman Bellic", "Viktor Bellic", " Nikolai Bellic", "Dimitri Rascalov"],
+    answer: "Roman Bellic",
+  },
+];
+
+export default data;
---
+++ src/data/questions_complete.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    category: "SENAI",
+    questions: [
+      {
+        question: "Qual melhor lugar do senai pra dormir?",
+        options: ["biblioteca", "na sala mesmo", "no chao da cantina", "na quadra"],
+        answer: "biblioteca",
+      },
+      {
+        question: "Melhor professor que ja passou pelo senai:",
+        options: ["Jackson", "Luiz", "Charles", "Rodrigo"],
+        answer: "Jackson",
+      },
+      {
+        question: "Melhor professor das materias normais:",
+        options: ["Aline", "Luciano", "Marcus(o demitido)", "Tai"],
+        answer: "Luciano",
+      },
+    ],
+  },
+  {
+    category: "GTA ",
+    questions: [
+      {
+        question: "O que é Vanilla JavaScript?",
+        options: [
+          "JavaScript puro",
+          "Uma biblioteca JavaScript",
+          "Um framework JavaScript",
+          "Um compilador de JavaScript",
+        ],
+        answer: "JavaScript puro",
+      },
+      {
+        question: "Com qual instrução declaramos uma constante em JavaScript?",
+        options: ["const", "let", "var", "define"],
+        answer: "const",
+      },
+      {
+        question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+        options: ["string", "number", "boolean", "float"],
+        answer: "float",
+      },
+      {
+        question: "Qual dos métodos a seguir seleciona um elemento?",
+        options: ["querySelector", "parseInt", "sort", "reduce"],
+        answer: "querySelector",
+      },
+      {
+        question:
+          "Qual destas propriedades da a quantidade de elementos de um array?",
+        options: ["qty", "length", "items", "index"],
+        answer: "length",
+      },
+    ],
+  },
+];
+
+export default data;
